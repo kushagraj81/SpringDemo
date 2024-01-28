d22f2131a23c07e4fe6fecf953b971e02325441b
Kushagra <kushagraj81@gmail.com>
2024-01-28 20:10:10 +0530

    simple bean demo

--- src/main/java/com/example/basiccontroller/KushDetailController.java
+++ src/main/java/com/example/basiccontroller/KushDetailController.java
@@ -2,6 +2,7 @@ package com.example.basiccontroller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +11,21 @@ public class KushDetailController {
     private static final Logger logger = LoggerFactory.getLogger(KushDetailController.class);
     @Value("${my.name}")
     private String myname;
+
+    private Mydetails mydetails;
+// If we only have one constructor then @Autowired over that constructor is optional
+    @Autowired
+    public KushDetailController(Mydetails mydetails)
+    {
+        this.mydetails=mydetails;
+    }
+
     @GetMapping("/")
     String personalDetails()
     {
 
         logger.info("Hello My friend {}----->",myname);
-        return "Hello Kushagra, you are great!!";
+        return mydetails.getKushDetails();
     }
 
 }
---
+++ src/main/java/com/example/basiccontroller/Mydetails.java
@@ -0,0 +1,10 @@
+package com.example.basiccontroller;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Mydetails {
+    public String getKushDetails(){
+        return "Kushagra is a very ambitious and strong willed man!!";
+    }
+}
