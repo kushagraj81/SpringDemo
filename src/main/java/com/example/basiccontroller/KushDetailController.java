4d9fba1296e67657931d3097c79dd0560f33ed5f
Kushagra <kushagraj81@gmail.com>
2024-01-28 13:07:26 +0530

    access value from porperty file

--- src/main/java/com/example/basiccontroller/KushDetailController.java
+++ src/main/java/com/example/basiccontroller/KushDetailController.java
@@ -2,16 +2,20 @@ package com.example.basiccontroller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class KushDetailController {
     private static final Logger logger = LoggerFactory.getLogger(KushDetailController.class);
+    @Value("${my.name}")
+    private String myname;
     @GetMapping("/")
     String personalDetails()
     {
-        logger.info("kushagra----->");
+
+        logger.info("Hello My friend {}----->",myname);
         return "Hello Kushagra, you are great!!";
     }
 
