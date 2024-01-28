1eef97384ae2ac2bb3f9107aa645a0067e914a08
Kushagra <kushagraj81@gmail.com>
2024-01-28 15:52:13 +0530

    spring security and logging level

--- src/main/java/com/example/basiccontroller/KushDetailController.java
+++ src/main/java/com/example/basiccontroller/KushDetailController.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 @RestController
 public class KushDetailController {
     private static final Logger logger = LoggerFactory.getLogger(KushDetailController.class);
