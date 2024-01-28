d57423f30c59bd60d8d38f1b9f012430244aa8c5
Kushagra <kushagraj81@gmail.com>
2024-01-28 13:02:55 +0530

    master commit

---
+++ src/main/java/com/example/basiccontroller/BasiccontrollerApplication.java
@@ -0,0 +1,13 @@
+package com.example.basiccontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasiccontrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasiccontrollerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/basiccontroller/BasiccontrollerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basiccontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasiccontrollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
