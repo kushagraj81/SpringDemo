ce9d0ac9380b8ebe8118b646c1d488fb8392fde9
Kushagra <kushagraj81@gmail.com>
2024-01-29 22:05:48 +0530

    bean lifecycle

--- src/main/java/com/example/basiccontroller/KushDetailController.java
+++ src/main/java/com/example/basiccontroller/KushDetailController.java
@@ -1,16 +1,27 @@
 package com.example.basiccontroller;
 
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.apache.coyote.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.http.HttpHeaders;
+import java.util.Collections;
+
 @RestController
 public class KushDetailController {
     private static final Logger logger = LoggerFactory.getLogger(KushDetailController.class);
     @Value("${my.name}")
     private String myname;
+
+    Boolean flag=false;
 
     private Mydetails mydetails;
 // If we only have one constructor then @Autowired over that constructor is optional
@@ -23,9 +34,24 @@ public class KushDetailController {
     @GetMapping("/")
     String personalDetails()
     {
-
         logger.info("Hello My friend {}----->",myname);
+
         return mydetails.getKushDetails();
+    }
+//    this function stops the whole thread for 5 seconds
+//    this is not our case. We want to check what happens when response is delayed for sometime.
+//    does or code moves to next line in such case or waits.
+    public Boolean changeFlagToTrue()
+    {
+        try {
+            Thread.sleep(5000);
+            logger.info("10 seconds has passed");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        logger.info("We are out of Changeflag function now");
+        return true;
+
     }
 
 }
--- src/main/java/com/example/basiccontroller/Mydetails.java
+++ src/main/java/com/example/basiccontroller/Mydetails.java
@@ -1,10 +1,22 @@
 package com.example.basiccontroller;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Mydetails {
     public String getKushDetails(){
         return "Kushagra is a very ambitious and strong willed man!!";
+    }
+    @PostConstruct
+    public void postConstruct()
+    {
+        System.out.println("post construct called---->" + getClass().getName());
+    }
+    @PreDestroy
+    public void predestroy()
+    {
+        System.out.println("pre destroy called ---> "+getClass().getName());
     }
 }
